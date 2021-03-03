190f14d3f4cfb61d247e4d1f4d8dbbac0caed4be
alexcharlton93 <alex-charlton@live.co.uk>
2021-03-03 09:00:16 +0000

    Added additional error handling

--- src/util/generateCSV/generateCSV.js
+++ src/util/generateCSV/generateCSV.js
@@ -1,6 +1,10 @@
 import { createObjectCsvWriter as createCsvWriter } from 'csv-writer';
 
 export const generateCSV = (header, data) => {
+    if (!header || !data) {
+        throw new Error("No data was specified to be converted into CSV");
+    }
+
     const csvWriter = createCsvWriter({
         path: 'src/report/currency-report.csv',
         header
--- src/util/getPreviousDates/getPreviousDates.js
+++ src/util/getPreviousDates/getPreviousDates.js
@@ -1,6 +1,10 @@
 import moment from 'moment';
 
 export const getPreviousDates = (amountOfDays) => {
+    if (!amountOfDays) {
+        throw new Error("Please specify the amount of days data to obtain");
+    }
+
     const dates = [];
     for (let i = 0; i < amountOfDays; i++) {
         dates.push(moment(new Date().setDate(new Date().getDate() - i)).format('YYYY-MM-DD'));
